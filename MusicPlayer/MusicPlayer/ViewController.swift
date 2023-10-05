3756118b9cb11ea4bd6754cea77b0e5a97b6e86c
RudinP <song_e@sookmyung.ac.kr>
2023-10-05 14:40:06 +0900

    MusicPlayer 프로젝트 추가

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 루딘 on 10/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
