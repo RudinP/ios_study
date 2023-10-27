dcdfb7ee01f03238c2e5dcbcfdb92c3bd858e1e5
RudinP <song_e@sookmyung.ac.kr>
2023-10-27 13:36:37 +0900

    AutoLayoutPractice 추가

---
+++ AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by 루딘 on 10/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
