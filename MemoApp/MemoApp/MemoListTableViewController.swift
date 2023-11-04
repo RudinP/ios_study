ab076ce7edddd0a78fb6712fd4e2acd638a0330f
RudinP <song_e@sookmyung.ac.kr>
2023-11-04 15:31:17 +0900

    DateFormatter 추가

--- MemoApp/MemoApp/MemoListTableViewController.swift
+++ MemoApp/MemoApp/MemoListTableViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class MemoListTableViewController: UITableViewController {
 
+    let formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .short
+        f.locale = Locale(identifier: "Ko_kr")
+        
+        return f
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -35,7 +44,7 @@ class MemoListTableViewController: UITableViewController {
         // Configure the cell...
         let target = Memo.dummyMemoList[indexPath.row]
         cell.textLabel?.text = target.content
-        cell.detailTextLabel?.text = target.insertDate.description
+        cell.detailTextLabel?.text = formatter.string(from: target.insertDate)
         
         return cell
     }
