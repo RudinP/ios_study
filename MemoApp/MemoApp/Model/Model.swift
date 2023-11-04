e9501ec5bb3572bc7e4433a460f9adc4093942bc
RudinP <song_e@sookmyung.ac.kr>
2023-11-04 14:30:16 +0900

    Model Class 추가

---
+++ MemoApp/MemoApp/Model/Model.swift
@@ -0,0 +1,25 @@
+//
+//  Model.swift
+//  MemoApp
+//
+//  Created by 루딘 on 11/4/23.
+//
+
+import Foundation
+
+class Memo{
+    var content: String //메모 내용
+    var insertDate: Date //메모 추가 날짜
+    
+    init(content: String) {
+        self.content = content
+        self.insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content:"오늘은 2023년 11월 4일 집에 가고싶다."),
+        Memo(content: "1"),
+        Memo(content: "2"),
+        Memo(content: "3")
+    ]
+}
