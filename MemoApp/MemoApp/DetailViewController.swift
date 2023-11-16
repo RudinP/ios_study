9bf9a1bd5bd5b3e0134b09ed9bddce7b5a30c7e7
RudinP <song_e@sookmyung.ac.kr>
2023-11-16 12:50:38 +0900

    <MemoApp> 세그웨이를 통한 데이터 전달로 DetailView에 메모 표시 구현

--- MemoApp/MemoApp/DetailViewController.swift
+++ MemoApp/MemoApp/DetailViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    var memo: Memo?
+    let formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .short
+        f.locale = Locale(identifier: "Ko_kr")
+        
+        return f
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,9 +47,11 @@ extension DetailViewController: UITableViewDataSource{
         switch indexPath.row{
         case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
+            cell.textLabel?.text = memo?.content
             return cell
         case 1:
             let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+            cell.textLabel?.text = formatter.string(for: memo?.insertDate)
             return cell
         default:
             fatalError()
--- MemoApp/MemoApp/MemoListTableViewController.swift
+++ MemoApp/MemoApp/MemoListTableViewController.swift
@@ -32,6 +32,14 @@ class MemoListTableViewController: UITableViewController {
         }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let cell = sender as? UITableViewCell, let indexPath = tableView.indexPath(for: cell){
+            if let vc = segue.destination as? DetailViewController{
+                vc.memo = Memo.dummyMemoList[indexPath.row]
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
