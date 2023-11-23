37864816363f445871a602ddb61800ed3b6e0ed9
RudinP <song_e@sookmyung.ac.kr>
2023-11-23 12:21:41 +0900

    <MemoApp> 편집 취소 확인 기능 추가

--- MemoApp/MemoApp/ComposeViewController.swift
+++ MemoApp/MemoApp/ComposeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ComposeViewController: UIViewController {
     
     var editTarget: Memo?
+    var originalMemoContent: String?
     
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -42,12 +43,49 @@ class ComposeViewController: UIViewController {
         if let memo = editTarget {
             navigationItem.title = "메모 편집"
             memoTextView.text = memo.content
+            originalMemoContent = memo.content
         } else {
             navigationItem.title = "새 메모"
             memoTextView.text = ""
         }
+        memoTextView.delegate = self
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.presentationController?.delegate = self
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.presentationController?.delegate = nil
+    }
+}
+
+extension ComposeViewController: UITextViewDelegate{
+    func textViewDidChange(_ textView: UITextView) {
+        if let original = originalMemoContent, let edited = textView.text{
+            isModalInPresentation = original != edited
+        }
+    }
+}
+
+extension ComposeViewController: UIAdaptivePresentationControllerDelegate{
+    func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
+        let alert = UIAlertController(title: "알림", message: "편집한 내용을 저장할까요?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: {[weak self] (action) in
+            self?.save(action)
+        })
+        alert.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: {[weak self] (action) in
+            self?.close(action)
+        })
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
 extension ComposeViewController{
