16f950044e1c20469bd9d2e644c924fdb006f262
RudinP <song_e@sookmyung.ac.kr>
2023-11-23 13:07:49 +0900

    <MemoApp> 메모 삭제 기능 구현

--- MemoApp/MemoApp/DetailViewController.swift
+++ MemoApp/MemoApp/DetailViewController.swift
@@ -21,6 +21,20 @@ class DetailViewController: UIViewController {
         return f
     }()
     
+    @IBAction func deleteMemo(_ sender: Any) {
+        let alert = UIAlertController(title: "삭제 확인", message: "메모를 삭제하시겠습니까?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "삭제", style: .destructive, handler: {[weak self] (action) in
+            DataManager.shared.deleteMemo(self?.memo)
+            self?.navigationController?.popViewController(animated: true)
+        })
+        alert.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let vc = segue.destination.children.first as? ComposeViewController {
             vc.editTarget = memo
