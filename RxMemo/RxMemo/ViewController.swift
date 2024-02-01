1b887881de927e45adb4845add599a804919b001
RudinP <song_e@sookmyung.ac.kr>
2024-02-01 14:20:02 +0900

    RxMemo init

---
+++ RxMemo/RxMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxMemo
+//
+//  Created by 루딘 on 2/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
