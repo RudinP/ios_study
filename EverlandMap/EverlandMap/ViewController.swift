cb323979d3f17f0e07f156129a1f8ddc6fe98446
RudinP <song_e@sookmyung.ac.kr>
2024-06-27 13:52:57 +0900

    step 4

--- EverlandMap/EverlandMap/ViewController.swift
+++ EverlandMap/EverlandMap/ViewController.swift
@@ -30,6 +30,49 @@ class ViewController: UIViewController {
         return results
     }
     
+    @IBAction func showAttractions(_ sender: Any) {
+        show(category: .attraction)
+    }
+    
+    @IBAction func showPerformance(_ sender: Any) {
+        show(category: .performance)
+    }
+    
+    @IBAction func showAmenities(_ sender: Any) {
+        show(category: .amenity)
+    }
+    
+    @IBAction func showGiftshops(_ sender: Any) {
+        show(category: .giftshop)
+    }
+    
+    @IBAction func showRestaurants(_ sender: Any) {
+        show(category: .restaurant)
+    }
+    func show(category: Category){
+        //어노테이션 삭제 방법
+        mapView.removeAnnotations(mapView.annotations)
+        
+        for obj in geoJsonObjects {
+            guard let feature = obj as? MKGeoJSONFeature else {continue}
+            let jsonDecoder = JSONDecoder()
+            
+            guard let pdata = feature.properties,
+                  let properties = try? jsonDecoder.decode(Property.self, from: pdata) else {continue}
+            
+            guard let objCategory = Category(rawValue: properties.category) else {continue}
+            //포인트면 위치 마킹
+            if let pointAnnotation = feature.geometry.first as? MKPointAnnotation {
+                if category == objCategory {
+                    //이부분 변경
+                    let annotation = EverlandAnnotation(coordinate: pointAnnotation.coordinate, properties: properties)
+                    //지도에 마킹
+                    mapView.addAnnotation(annotation)
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         menuContainerView.layer.cornerRadius = 20
@@ -42,22 +85,6 @@ class ViewController: UIViewController {
         Task{
             geoJsonObjects = try await fetchMap()
             
-            for obj in geoJsonObjects {
-                guard let feature = obj as? MKGeoJSONFeature else {continue}
-                let jsonDecoder = JSONDecoder()
-                guard let pdata = feature.properties,
-                      let properties = try? jsonDecoder.decode(Property.self, from: pdata) else {continue}
-                guard let category = Category(rawValue: properties.category) else {continue}
-                //포인트면 위치 마킹
-                if let pointAnnotation = feature.geometry.first as? MKPointAnnotation {
-                    if category == .attraction {
-                        //이부분 변경
-                        let annotation = EverlandAnnotation(coordinate: pointAnnotation.coordinate, properties: properties)
-                        //지도에 마킹
-                        mapView.addAnnotation(annotation)
-                    }
-                }
-            }
         }
     }
 }
@@ -71,6 +98,8 @@ extension ViewController: MKMapViewDelegate{
             let marker = mapView.dequeueReusableAnnotationView(withIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier, for: annotation) as! MKMarkerAnnotationView
             //마커의 그리프이미지 변경
             marker.glyphImage = everlandAnnotation.image
+            //마커 추가 시 바운스애니메이션 추가
+            marker.animatesWhenAdded = true
             //pointannotation에는 title, subtitle, 좌표 세 가지가 저장된다.
             //따라서 우리가 설정한 카테고리에 접근하려면 커스텀 어노테이션을 만들고 어노테이션에 프로퍼티를 함께 저장해야 함.
             return marker
