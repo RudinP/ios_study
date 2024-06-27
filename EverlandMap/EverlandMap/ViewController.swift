20b9e1ab3f4b4fca7eb9ac4978faae18e163687b
RudinP <song_e@sookmyung.ac.kr>
2024-06-27 11:44:34 +0900

    step 2

--- EverlandMap/EverlandMap/ViewController.swift
+++ EverlandMap/EverlandMap/ViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var menuContainerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        menuContainerView.layer.cornerRadius = 20
+        menuContainerView.clipsToBounds = true
+        
+        let center = CLLocationCoordinate2D(latitude: 37.294259, longitude: 127.2039509)
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 300, longitudinalMeters: 300)
+        mapView.setRegion(region, animated: false)
     }
 
 
